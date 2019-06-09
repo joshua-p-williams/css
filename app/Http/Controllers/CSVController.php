41b5d59e67f89b01be54c6969f40ba1857a27f97
Joshua Williams <josh@timitek.com>
2019-06-09 17:03:42 -0500

    Show the extra columns on the export

--- app/Http/Controllers/CSVController.php
+++ app/Http/Controllers/CSVController.php
@@ -16,8 +16,8 @@ class CSVController extends Controller
     }
 
     public function allResults() {
-        $results = AllResults::get(); // All users
+        $results = AllResults::get();
         $csvExporter = new \Laracsv\Export();
-        $csvExporter->build($results, ['category_name', 'team_name', 'participant_name', 'archery', 'shotgun', 'rifle', 'muzzleloader', 'responsibility_exam', 'safety_trail', 'wildlife_id', 'orienteering'])->download();
+        $csvExporter->build($results, ['category_name', 'team_name', 'participant_name', 'total_score', 'tie_breaker_1', 'tie_breaker_2', 'tie_breaker_3', 'tie_breaker_4', 'archery', 'shotgun', 'rifle', 'muzzleloader', 'responsibility_exam', 'safety_trail', 'wildlife_id', 'orienteering'])->download();
     }
 }
