eb487dea4dae0e948b5469a7df6392a780ee9be7
Joshua Williams <josh@timitek.com>
2019-06-09 17:08:24 -0500

    Added the new columns to the migration

--- database/migrations/2019_06_09_214414_alter_all_results_view.php
+++ database/migrations/2019_06_09_214414_alter_all_results_view.php
@@ -14,10 +14,15 @@ class AlterAllResultsView extends Migration
     public function up()
     {
         DB::statement("
-        create or replace view v_all_results as
+        create or replace view v_all_results as        
         select category_name
         , team_name
         , participant_name
+        , MAX(total_score) as total_score
+        , MAX(tie_breaker_1) as tie_breaker_1
+        , MAX(tie_breaker_2) as tie_breaker_2
+        , MAX(tie_breaker_3) as tie_breaker_3
+        , MAX(tie_breaker_4) as tie_breaker_4
         , sum(archery) as archery
         , sum(shotgun) as shotgun
         , sum(rifle) as rifle
@@ -30,6 +35,11 @@ class AlterAllResultsView extends Migration
             select category_name
             , team_name
             , participant_name
+            , sum(score) as total_score
+            , MAX(tie_breaker_1) as tie_breaker_1
+            , MAX(tie_breaker_2) as tie_breaker_2
+            , MAX(tie_breaker_3) as tie_breaker_3
+            , MAX(tie_breaker_4) as tie_breaker_4
             , MAX(IF(event_name = 'Archery', score, NULL)) AS archery
             , MAX(IF(event_name = 'Shotgun', score, NULL)) AS shotgun
             , MAX(IF(event_name = 'Rifle', score, NULL)) AS rifle
@@ -47,9 +57,11 @@ class AlterAllResultsView extends Migration
         group by category_name
         , team_name
         , participant_name
-        order by category_name
-        , team_name
-        , participant_name
+        order by total_score DESC
+        , tie_breaker_1 DESC
+        , tie_breaker_2 DESC
+        , tie_breaker_3 DESC
+        , tie_breaker_4 DESC
         ");
     }
 
