4af4c77b676ae6d0bbf1efb743fb2b756b88c815
Joshua Williams <josh@timitek.com>
2019-06-15 23:41:34 -0500

    Make the team export include the keep_top_scores

--- app/Http/Controllers/CSVController.php
+++ app/Http/Controllers/CSVController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\ParticipantList;
 use App\IndividualRanking;
+use App\TeamRanking;
 
 class CSVController extends Controller
 {
@@ -15,7 +16,7 @@ class CSVController extends Controller
         $csvExporter->build($participants, ['category', 'team', 'participant'])->download();
     }
 
-    public function resultsSummary() {
+    public function individualRankingSummary() {
         $results = [];
         $fields = [];
         $events = [];
@@ -60,40 +61,57 @@ class CSVController extends Controller
             'events' => $events,
         ];
     }
-    
-    public function teamResults() {
-        $results = [];
-        $resultsSummary = $this->resultsSummary();
 
-        $categories = $resultsSummary['results']->groupBy(['Category', 'Team']);
+    public function individualResults() {
+        $summary = $this->individualRankingSummary();
+        $csvExporter = new \Laracsv\Export();
+        $csvExporter->build($summary['results'], $summary['fields'])->download();
+    }
+    
+
+    public function teamRankingSummary() {
+        $results = [];
+        $fields = [];
+        $events = [];
+
+        $categories = TeamRanking::get()->groupBy(['category_name', 'team_name']);
+
         foreach($categories as $category => $teams) {
             foreach($teams as $team => $scores) {
                 $summary = [
                     'Category' => $category,
                     'Team' => $team,
-                    'Total Score' => $scores->sum('Total Score'),
-                    'Tie Breaker 1' => $scores->sum('Tie Breaker 1'),
-                    'Tie Breaker 2' => $scores->sum('Tie Breaker 2'),
-                    'Tie Breaker 3' => $scores->sum('Tie Breaker 3'),
-                    'Tie Breaker 4' => $scores->sum('Tie Breaker 4'),
+                    'Total Score' => $scores->sum('score'),
+                    'Tie Breaker 1' => $scores->max('tie_breaker_1'),
+                    'Tie Breaker 2' => $scores->max('tie_breaker_2'),
+                    'Tie Breaker 3' => $scores->max('tie_breaker_3'),
+                    'Tie Breaker 4' => $scores->max('tie_breaker_4'),
                 ];
 
-                foreach($resultsSummary['events'] as $event) {
-                    $summary[$event] = $scores->sum($event);
+                $eventScores = $scores->groupBy('event_name');
+
+                if (empty($fields)) {
+                    $events = $eventScores->keys()->toArray();
+                    $fields = array_merge(array_keys($summary), $events);
+                }
+
+                foreach($eventScores as $event => $eventScores) {
+                    $summary[$event] = $eventScores->sum('score');
                 }
 
                 $results[] = $summary;
             }
         }
 
-        $fields = array_merge(['Category', 'Team', 'Total Score', 'Tie Breaker 1', 'Tie Breaker 2', 'Tie Breaker 3', 'Tie Breaker 4'], $resultsSummary['events']);
-
-        $csvExporter = new \Laracsv\Export();
-        $csvExporter->build(collect($results), $fields)->download();
+        return [
+            'results' => collect($results),
+            'fields' => $fields,
+            'events' => $events,
+        ];
     }
 
-    public function individualResults() {
-        $summary = $this->resultsSummary();
+    public function teamResults() {
+        $summary = $this->teamRankingSummary();
         $csvExporter = new \Laracsv\Export();
         $csvExporter->build($summary['results'], $summary['fields'])->download();
     }
