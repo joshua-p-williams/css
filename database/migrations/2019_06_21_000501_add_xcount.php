4fbea9832023b478cadbbe8ca78e4ac8e85387ac
Joshua Williams <joshua.williams@nephosintegration.com>
2019-06-20 21:00:39 -0500

    Added the view logic for including xcount in tie breaker 1

--- database/migrations/2019_06_21_000501_add_xcount.php
+++ database/migrations/2019_06_21_000501_add_xcount.php
@@ -20,6 +20,59 @@ class AddXcount extends Migration
         Schema::table('settings', function (Blueprint $table) {
             $table->boolean('xcount_for_tb')->default(false);
         });
+
+        DB::statement("
+        create or replace view v_individual_tie_breaker_1 as
+        select
+        tb.tie_breaker_name ,
+        tb.category_id , 
+        tb.category_name ,
+        tb.participant_id , 
+        tb.participant_name ,
+        sum(COALESCE(tb.score, 0)) as score
+    from (
+        select
+            e.name as tie_breaker_name ,
+            cat.id as category_id ,
+            cat.name as category_name ,
+            c.id as participant_id ,
+            c.name as participant_name ,
+            SUM(COALESCE(s.xcount, 0)) as score
+        from events e
+        join participants c
+        left join categories cat on c.category_id = cat.id
+        left join scores s on c.id = s.participant_id and e.id = s.event_id
+        where e.deleted_at is null
+        and cat.deleted_at is null
+        and s.deleted_at is null
+        and (select xcount_for_tb from settings order by id limit 1) <> 0
+        group by e.name, cat.id, cat.name, c.id, c.name         
+        UNION
+        select
+            e.name as tie_breaker_name ,
+            cat.id as category_id ,
+            cat.name as category_name ,
+            c.id as participant_id ,
+            c.name as participant_name ,
+            SUM(COALESCE(s.score, 0)) as score
+        from events e
+        join participants c
+        left join categories cat on c.category_id = cat.id
+        left join scores s on c.id = s.participant_id and e.id = s.event_id
+        where e.deleted_at is null
+        and cat.deleted_at is null
+        and s.deleted_at is null
+        and e.id in (select id from events where use_in_tb_1 = 1)
+        group by e.name, cat.id, cat.name, c.id, c.name         
+    ) tb
+    group by
+        tb.tie_breaker_name ,
+        tb.category_id , 
+        tb.category_name ,
+        tb.participant_id , 
+        tb.participant_name
+        ");
+
     }
 
     /**
@@ -36,5 +89,7 @@ class AddXcount extends Migration
         Schema::table('settings', function (Blueprint $table) {
             $table->dropColumn('xcount_for_tb');
         });
+
+        (new CreateSummaryViews())->up();
     }
 }
